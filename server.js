1f71ea9e6e55eaaab27b3aa46aa42f5d2a7cdaf3
kumar <santanupaikaray23@gmail.com>
2021-10-26 12:02:16 +0530

    add all files

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT||9999;
+const port = process.env.PORT||9800;
 
 app.use(express.static(path.join(__dirname,'build')));
 
